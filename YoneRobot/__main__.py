09de76a480fdd4b0d055012280e7d86db7796d3c
aman706 <76611203+aman706@users.noreply.github.com>
2021-08-25 11:06:41 +0530

    Update __main__.py

--- YoneRobot/__main__.py
+++ YoneRobot/__main__.py
@@ -349,16 +349,16 @@ def yone_about_callback(update, context):
     query = update.callback_query
     if query.data == "ironman_":
         query.message.edit_text(
-            text=""" ℹ️ I'm *yone*, a powerful group management bot built to help you manage your group easily.
+            text=""" ℹ️ I'm *ironman *, a powerful group management bot built to help you manage your group easily.
                  \n❍ I can restrict users.
                  \n❍ I can greet users with customizable welcome messages and even set a group's rules.
                  \n❍ I have an advanced anti-flood system.
                  \n❍ I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
                  \n❍ I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
                  \n❍ I check for admins' permissions before executing any command and more stuffs
-                 \n\n_yone's licensed under the GNU General Public License v3.0_
+                 \n\n_ironman's licensed under the GNU General Public License v3.0_
                  \nHere is the [💾Repository](https://github.com/aman706/Ironman2.o).
-                 \n\nIf you have any question about yone, let us know at .""",
+                 \n\nIf you have any question about ironman, let us know at .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
